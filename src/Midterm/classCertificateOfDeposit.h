d8fa1e2671d50cd893616477ed5725453cce5aba
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 21:22:07 +0000

    CD complete

--- src/Midterm/classCertificateOfDeposit.h
+++ src/Midterm/classCertificateOfDeposit.h
@@ -4,8 +4,27 @@ using namespace std;
 
 class certificateOfDeposit: public bankAccount{
     public:
+        certificateOfDeposit(string first, string last, int acctNum, double acctBal, double interest = 0.08, int maturityMonths = 6, double earlyPenalty = 0.05)
+        :bankAccount(first,last,acctNum,acctBal),interest(interest),maturityMonths(maturityMonths),currMaturity(1),earlyPenalty(earlyPenalty){}
+
+        void accountDeposit(double amount) override;
+        void accountWithdraw(double amount) override;
+
     private:
         int maturityMonths;
         int currMaturity;
-        int interest;        
+        double interest;        
+        double earlyPenalty;
 };
+
+void certificateOfDeposit::accountDeposit(double amount){
+    if(amount < 0){cerr<<"Cannot deposit negative amount" << endl; return;}
+    else{setAccountBal(getAccountBal() + amount);}
+}
+
+void certificateOfDeposit::accountWithdraw(double amount){
+    if(getAccountBal() < amount || amount < 0){cerr<<"Invalid withdraw amount"<<endl; return;}
+    if(currMaturity == maturityMonths){setAccountBal(getAccountBal() - amount);}
+    else{setAccountBal((getAccountBal() - amount)-(amount*earlyPenalty));}
+
+}
