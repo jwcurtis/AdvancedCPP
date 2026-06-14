adbfba5f486f7623fe21bec5561ca6773f4bb040
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 20:40:56 +0000

    highInterestChecking complete

--- src/Midterm/classHighInterestChecking.h
+++ src/Midterm/classHighInterestChecking.h
@@ -6,8 +6,16 @@ using namespace std;
 
 class highInterestCheckingAccount: public noServiceChargeCheckingAccount{
     public:
-    private:
-        int interest;
-        int minBalance;
-        
+    highInterestCheckingAccount(string first, string last, int acctNum, double acctBal, double interest = 0.15, double minBal = 1500)
+    :noServiceChargeCheckingAccount(first, last, acctNum, acctBal,interest,minBal){}
+
+    void createStatement() override;
 };
+
+void highInterestCheckingAccount::createStatement(){
+    cout << "Account statement for: " << getName() << endl;
+    cout << "Account type: High Interest Checking Account" << endl;
+    cout << "Account number: " << getAccountNum() << endl;
+    cout << "Account balance: " << getAccountBal() << endl;
+    cout << "Account interest: " << (getInterest()) << endl;
+}
