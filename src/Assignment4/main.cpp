7e2af385e00a7e36a31fbf382bf84248ce76e0d7
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 01:09:01 +0000

    Exception class init

--- src/Assignment4/main.cpp
+++ src/Assignment4/main.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 
+class negativeNum{};
+
 int main(){
     int feet,inches;
 
     try{
 
         cout << "Input feet and inches to be converted to centimeters." << endl;
+
+        cin >> feet >> inches;
+
+        if(feet < 0 || inches < 0){throw negativeNum();}
     }
     catch(negativeNum error){
 
