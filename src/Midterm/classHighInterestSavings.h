bd95056320f63ed83d2bb8bb3c23325997082ad8
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 21:11:19 +0000

    Saving accounts setup

--- src/Midterm/classHighInterestSavings.h
+++ src/Midterm/classHighInterestSavings.h
@@ -5,7 +5,36 @@ using namespace std;
 
 class highInterestSavingsAccount: public savingsAccount{
     public:
+        highInterestSavingsAccount(string first, string last, int acctNum, double acctBal, double interest = 0.025, double minBal = 250)
+        :savingsAccount(first,last,acctNum,acctBal,interest), minBalance(minBal){}
+
+        void accountWithdraw(double amount) override;
+        void createStatement() override;
+
     private:
-        int minBalance;
-        int interest;        
+        double minBalance;      
 };
+
+void highInterestSavingsAccount::createStatement(){
+    cout << "Account statement for: " << getName() << endl;
+    cout << "Account type: Savings" << endl;
+    cout << "Account number: " << getAccountNum() << endl;
+    cout << "Account balance: " << getAccountBal() << endl;
+    cout << "Account interest: " << (getInterest()) << endl;
+    cout << "Minimum balance: " << minBalance << endl;
+}
+
+void highInterestSavingsAccount::accountWithdraw(double amount){
+    try
+    {
+        if((getAccountBal() - amount) < minBalance){
+            throw"Cannot withdraw past the minimum account balance.";
+        }
+        else{setAccountBal(getAccountBal() - amount);}
+    }
+    catch(const char* error)
+    {
+        cerr << error << endl;
+    }
+    
+}
