929da1134a4fdbeec64f876f940d9e87b2a15c31
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 00:49:07 +0000

    Start test program

--- src/Assignment3/main.cpp
+++ src/Assignment3/main.cpp
@@ -4,5 +4,20 @@ using namespace std;
 #include "rectangleClass.h"
 
 int main(){
+    rectangleType rect1(27,53);
+    rectangleType rect2(5,2);
+    rectangleType rect3(18,41);
+    rectangleType rect4(8,19);
+    rectangleType rect5;
+
+    cout << "Empty rectangle: " << rect5 << endl;
+    cout << "Input legnth and width for empty rectangle." << endl;
+    cin >> rect5;
+    cout << "Updated rectangle values: " << rect5 << endl;
+
+    cout << "Length: " << rect5.getLength() << ". Width: " << rect5.getWidth() << endl;
+    cout << "Area: " << rect5.area() << ". Perimeter: " << rect5.perimeter() << endl;
+    cout << "Print: " << rect5 << endl;
+
     
 }
