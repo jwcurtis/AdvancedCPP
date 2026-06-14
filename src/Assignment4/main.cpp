86b9d75cf5cab2f8d4d353d4553094e611678564
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-14 01:03:04 +0000

    Assignment 4 setup.

---
+++ src/Assignment4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int feet,inches;
+
+    try{
+
+        cout << "Input feet and inches to be converted to centimeters." << endl;
+    }
+    catch(negativeNum error){
+
+    }
+
+}
