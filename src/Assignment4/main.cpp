42e5b67496d6bbe195de842fa90a52d05a37e50b
Jacob <jwcurtis@tamu.edu>
2026-06-15 20:18:01 -0400

    Finished assignment 4

--- src/Assignment4/main.cpp
+++ src/Assignment4/main.cpp
@@ -4,23 +4,50 @@
 
 using namespace std;
 
-double inputFeet(int feet){
+double inputFeet(){
+    double feet;
+    cout << "Enter feet: ";
+    cin >> feet;
+    if(cin.fail()){cin.clear();
+        cin.ignore(10000, '\n');
+        throw convertException("Invalid input. Please enter a valid number");}
     if(feet < 0){ throw convertException("Negative numbers are not allowed");}
+    return feet;
 }
 
-double inputInches(int inches){
+double inputInches(){
+    double inches;
+    cout << "Enter inches: ";
+    cin >> inches;
+    if(cin.fail()){cin.clear();
+        cin.ignore(10000, '\n');
+        throw convertException("Invalid input. Please enter a valid number");}
     if(inches < 0){ throw convertException("Negative numbers are not allowed");}
+    return inches;
+}
+
+double convertToCentimeters(double feet, double inches){
+    double totalInches = feet * 12 + inches;
+    return totalInches * 2.54;
 }
 
 int main(){
+    
+    double feet, inches;
 
-
-    try{
-
-        
+    while(true){
+        try{
+            feet = inputFeet();
+            inches = inputInches();
+            cout << "You entered " << feet << " feet and " << inches << " inches." << endl;
+            break;
+        }
+        catch(convertException error){
+        cout << error.what() << ". Please try again." << endl;
+        }
     }
-    catch(convertException error){
-        cout << error.what() << " caugh in main." << endl;
-    }
+    
+    double centimeters = convertToCentimeters(feet, inches);
+    cout << "This is equivalent to " << centimeters << " centimeters." << endl;
 
 }
