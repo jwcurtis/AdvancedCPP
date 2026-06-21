13b65d9c3cbee67fb4054e43315b6e02ad867181
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-21 01:04:17 +0000

    Stack main tests

--- src/Assignment6/classStackType.h
+++ src/Assignment6/classStackType.h
@@ -1,10 +1,26 @@
-template <class Type>
+#ifndef H_StackType
+#define H_StackType
 
+#include <iostream>
+#include <cassert>
+
+#include "classStackADT.h"
+
+using namespace std;
+
+template <class Type>
 class stackType: public stackADT<Type>
 {
+    friend ostream& operator<<(ostream&, const stackType<Type>&);
     public:
 
         const stackType<Type>& operator=(const stackType<Type>&);
+
+        bool operator==(const stackType<Type>&) const;
+        bool operator!=(const stackType<Type>&) const;
+
+
+
         void initializeStack();
         bool isEmptyStack() const;
         bool isFullStack() const;
@@ -13,7 +29,7 @@ class stackType: public stackADT<Type>
         void pop();
 
         stackType(int stackSize = 100);
-        steakType(const stackType<Type>& otherStack);
+        stackType(const stackType<Type>& otherStack);
         ~stackType();
 
     private:
@@ -23,3 +39,131 @@ class stackType: public stackADT<Type>
 
         void copyStack(const stackType<Type>& otherStack);
 };
+
+template <class Type>
+void stackType<Type>::initializeStack(){
+    stackTop = 0;
+}
+
+template <class Type>
+bool stackType<Type>::isEmptyStack() const{
+    return (stackTop == 0);
+}
+
+template <class Type>
+bool stackType<Type>::isFullStack() const{
+    return(stackTop == maxStackSize);
+}
+
+template <class Type>
+void stackType<Type>::push(const Type& newItem){
+    if(! isFullStack()){
+        list[stackTop] = newItem;
+        stackTop++;
+    }
+    else{
+        cout << "Cannot add to a full stack." << endl;
+    }
+}
+
+template <class Type>
+Type stackType<Type>::top() const{
+    assert(stackTop != 0);
+    return list[stackTop - 1];
+}
+
+template <class Type>
+void stackType<Type>::pop(){
+    if(! isEmptyStack()){
+        stackTop--;
+    }
+    else{
+        cout << "Cannot remove from an empty stack." << endl;
+    }
+}
+
+template <class Type>
+stackType<Type>::stackType(int stackSize){
+    if(stackSize <= 0){
+        cout << "Size of the array to hold the stack must "
+        << "be positive." << endl;
+        cout << "Creating an array of size 100." << endl;
+        maxStackSize = 100;
+    }
+    else{maxStackSize = stackSize;}
+    stackTop = 0;
+    list = new Type[maxStackSize];
+}
+
+template <class Type>
+stackType<Type>::~stackType(){
+    delete [] list;
+}
+
+template <class Type>
+void stackType<Type>::copyStack(const stackType<Type>& otherStack){
+    delete [] list;
+    maxStackSize = otherStack.maxStackSize;
+    stackTop = otherStack.stackTop;
+
+    list = new Type[maxStackSize];
+    for(int i = 0; i < stackTop; i++){
+        list[i] = otherStack.list[i];
+    }
+}
+
+template <class Type>
+stackType<Type>::stackType(const stackType<Type>& otherStack){
+    list = nullptr;
+    copyStack(otherStack);
+}
+
+template <class Type>
+const stackType<Type>& stackType<Type>::operator=(const stackType<Type>& otherStack){
+    if(this != &otherStack){
+        copyStack(otherStack);
+    }
+    return *this;
+}
+
+template <class Type>
+bool stackType<Type>::operator==(const stackType<Type>& otherStack) const{
+
+    if(this == &otherStack){
+        return true;
+    }
+
+    if(stackTop != otherStack.stackTop){
+        return false;
+    }
+
+    for(int i = 0; i < stackTop; ++i){
+        if(list[i] != otherStack.list[i]){
+            return false;
+        }
+    }
+    
+
+    return true;
+}
+
+template <class Type>
+bool stackType<Type>::operator!=(const stackType<Type>& otherStack) const{
+    return !(*this == otherStack);
+}
+
+template <class Type>
+ostream& operator<<(ostream& osObject, const stackType<Type>& stack){
+    
+    osObject << "Stack values: ";
+    for(int i = stack.stackTop - 1; i >= 0; --i){
+
+        osObject << stack.list[i];
+
+        if(i != 0)
+            osObject << ", ";
+    }
+    return osObject;
+}
+
+#endif
--- src/Assignment6/main.cpp
+++ src/Assignment6/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 #include "classStackType.h"
 
+void testCopyConstructor(stackType<int> otherStack);
+
 int main(){
 
+    stackType<int> stack(50);
+    stackType<int> cpyStack(50);
+    stackType<int> dummyStack(100);
+
+    stack.initializeStack();
+    stack.push(85);
+    stack.push(25);
+    stack.push(56);
+    cpyStack = stack;
+
+    cout << "cpyStack: " << cpyStack << endl;
+
+    cpyStack = stack;
+    testCopyConstructor(stack);
+
+    if(!stack.isEmptyStack()){
+        cout << "The original stack is not empty." << endl 
+            << "The top element of the original stack: " 
+            << cpyStack.top() << endl;
+    }
+
+    dummyStack = stack;
+    cout << "Dummystack: " <<dummyStack << endl;
 
     return 0;
+}
+
+void testCopyConstructor(stackType<int> otherStack){
+    if (!otherStack.isEmptyStack()){
+        cout<< "otherStack is not empty." << endl  
+            << "The top element of otherStack: "
+            << otherStack.top() << endl;
+    }
 }
