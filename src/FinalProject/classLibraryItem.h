fe26f1ae5c009986574d625c287f5a4d1f899558
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-23 23:06:35 +0000

    Library class setup

--- src/FinalProject/classLibraryItem.h
+++ src/FinalProject/classLibraryItem.h
@@ -4,12 +4,52 @@ using namespace std;
 
 class libraryItem{
     public:
-        virtual void setName(string name) = 0;
-        virtual void setId(int id) = 0;
+        void setName(string name);
+        void setId(int id);
+        void setYear(int year);
 
-        virtual string getName() = 0;
-        virtual int getId() = 0;
+        string getName() const;
+        int getId() const;
+        int getYear() const;
+
+        virtual void getType() = 0;
+        virtual void display() = 0;
+
+        libraryItem(string name, int id, int year);
+        virtual ~libraryItem();
+
     private:
         string name;
         int id;
+        int year;
 };
+
+void libraryItem::setName(string inName){
+    name = inName;
+}
+
+void libraryItem::setId(int inId){
+    id = inId;
+}
+
+void libraryItem::setYear(int inYear){
+    year = inYear;
+}
+
+string libraryItem::getName() const{
+    return name;
+}
+
+int libraryItem::getId() const{
+    return id;
+}
+
+int libraryItem::getYear() const{
+    return year;
+}
+
+libraryItem::libraryItem(string name, int id, int year){
+    this-> name = name;
+    this-> id = id;
+    this-> year = year;
+}
