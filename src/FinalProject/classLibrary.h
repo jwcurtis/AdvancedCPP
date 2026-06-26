f4947fc1b55eccee07a989dca7fa79894ceef027
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-26 01:44:42 +0000

    Sort addition

--- src/FinalProject/classLibrary.h
+++ src/FinalProject/classLibrary.h
@@ -8,6 +8,10 @@ using namespace std;
 
 class Library{
     public:
+
+        Library();
+        ~Library();
+
         void addItem(libraryItem* newItem);
         void removeItem(int id);
 
@@ -19,7 +23,14 @@ class Library{
     private:
         vector<libraryItem*> items;
         stack<string> undoStack;
+        int undoLimit;
 };
+
+Library::~Library(){
+    for(libraryItem* item : items){
+        delete item;
+    }
+}
 
 void Library::addItem(libraryItem* newItem){
     items.push_back(newItem);
@@ -36,6 +47,7 @@ void Library::removeItem(int id){
             return;
         }
     }
+    throw runtime_error("Error deleting object!");
 }
 
 libraryItem* Library::searchById(int id){
@@ -54,4 +66,30 @@ libraryItem* Library::searchByName(string name){
         }
     }
     throw runtime_error("No item with that name found!");
+}
+
+void Library::sortByYear(){
+    libraryItem* temp;
+    for (size_t iteration = 1; iteration < items.size(); iteration++){
+        for( size_t index = 0; index < items.size() - iteration; index++){
+            if(items[index]->getYear() > items[index + 1]->getYear()){
+                temp = items[index];
+                items[index] = items[index + 1];
+                items[index + 1] = temp;
+            }
+        }
+    }
+}
+
+void Library::sortById(){
+    libraryItem* temp;
+    for (size_t iteration = 1; iteration < items.size(); iteration++){
+        for( size_t index = 0; index < items.size() - iteration; index++){
+            if(items[index]->getId() > items[index + 1]->getId()){
+                temp = items[index];
+                items[index] = items[index + 1];
+                items[index + 1] = temp;
+            }
+        }
+    }
 }
---
+++ src/FinalProject/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "classUser.h"
+#include "classLibrary.h"
+
+int main(){
+
+    return 0;
+}
