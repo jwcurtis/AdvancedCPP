cc28b32805fef1dcf4ff5a937af4fd17c754f6f4
jwcurtis <179052818+jwcurtis@users.noreply.github.com>
2026-06-28 18:23:47 +0000

    Main test functions

---
+++ src/Midterm/main.cpp
@@ -0,0 +1,17 @@
+#include "classBankAccount.h"
+#include "classCertificateOfDeposit.h"
+#include "classChecking.h"
+#include "classHighInterestChecking.h"
+#include "classHighInterestSavings.h"
+#include "classNoServiceChargeChecking.h"
+#include "classSavings.h"
+#include "classServiceChargeChecking.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
